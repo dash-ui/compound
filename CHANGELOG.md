a160ac3310445e27efd860a31372d0469b0aabf9
Jared Lunde <jared.lunde@gmail.com>
2021-09-13 21:18:59 -0600

    chore(release): 0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,14 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
-### 0.1.1 (2021-08-23)
-
+### [0.1.2](https://github.com/dash-ui/compound/compare/v0.1.1...v0.1.2) (2021-09-14)
 
 ### Features
 
-* make it work ([ed5c946](https://github.com/dash-ui/compound/commit/ed5c946cbd01b4d69f571c84adc4b5bf4c2987a0))
+- add atomic css options ([29c026f](https://github.com/dash-ui/compound/commit/29c026f65bd1f406ba2590289e1d84b5b13ac62e))
+
+### 0.1.1 (2021-08-23)
+
+### Features
+
+- make it work ([ed5c946](https://github.com/dash-ui/compound/commit/ed5c946cbd01b4d69f571c84adc4b5bf4c2987a0))
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,55 @@
-import type { ResponsiveLazy, ResponsiveOne, ResponsiveStyle, ResponsiveStyles } from '@dash-ui/responsive';
-import type { Style, Styles, StylesLazy, StylesOne, DashTokens } from '@dash-ui/styles';
+import type {
+  ResponsiveLazy,
+  ResponsiveOne,
+  ResponsiveStyle,
+  ResponsiveStyles,
+} from '@dash-ui/responsive'
+import type {
+  Style,
+  Styles,
+  StylesLazy,
+  StylesOne,
+  DashTokens,
+} from '@dash-ui/styles'
 /**
  * A factory function that creates a compound styles utility
  */
-declare function compound<Tokens extends DashTokens, ThemeNames extends string>(styles: Styles<Tokens, ThemeNames> | ResponsiveStyles<Tokens, any, ThemeNames>): <Keys extends string, T extends Record<Keys, StylesOne | ResponsiveStyle<any, any, any> | Style<any, DashTokens> | ResponsiveOne<any> | StylesLazy<any> | ResponsiveLazy<any, any>>, StyleMap extends { [Name in keyof T]: T[Name]; }>(styleMap: StyleMap) => ((compoundMap?: { [Name_1 in keyof StyleMap]?: Parameters<StyleMap[Name_1]>[0] | undefined; }) => string) & {
-    css: (compoundMap: { [Name_2 in keyof StyleMap]?: Parameters<StyleMap[Name_2]>[0] | undefined; }) => string;
-    styles: StyleMap;
-};
-export default compound;
+declare function compound<Tokens extends DashTokens, ThemeNames extends string>(
+  styles: Styles<Tokens, ThemeNames> | ResponsiveStyles<Tokens, any, ThemeNames>
+): <
+  Keys extends string,
+  T extends Record<
+    Keys,
+    | StylesOne
+    | ResponsiveStyle<any, any, any>
+    | Style<any, DashTokens>
+    | ResponsiveOne<any>
+    | StylesLazy<any>
+    | ResponsiveLazy<any, any>
+  >,
+  StyleMap extends {[Name in keyof T]: T[Name]}
+>(
+  styleMap: StyleMap,
+  options?: CompoundStylesOptions
+) => ((
+  compoundMap?: {
+    [Name_1 in keyof StyleMap]?: Parameters<StyleMap[Name_1]>[0] | undefined
+  },
+  compoundOptions?: CompoundStylesOptions
+) => string) & {
+  css: (
+    compoundMap: {
+      [Name_2 in keyof StyleMap]?: Parameters<StyleMap[Name_2]>[0] | undefined
+    }
+  ) => string
+  atomicCss: (
+    compoundMap: {
+      [Name_3 in keyof StyleMap]?: Parameters<StyleMap[Name_3]>[0] | undefined
+    }
+  ) => string[]
+  styles: StyleMap
+}
+export declare type CompoundStylesOptions = {
+  atomic?: boolean
+}
+export default compound
